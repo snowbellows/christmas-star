c6c24e94089365d3f36013d79a7a17d98d941109
Alex Halverson <alexhalverson@posteo.net>
2025-11-22 09:59:34 +1100

    feat: draw star

--- sketch.js
+++ sketch.js
@@ -1,7 +1,53 @@
+const YELLOW = '#ffc400ff';
+const PURPLE = '#9c27b0ff';
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 848);
 }
 
 function draw() {
-  background(220);
+  // background(220);
+
+  push();
+  translate(width / 2, height / 2);
+  drawStar(5);
+  pop();
+}
+
+function drawStar(layers) {
+  for (let i = 0; i < layers; i++) {
+    push();
+    if (i % 2 == 0) {
+      stroke(YELLOW);
+    } else {
+      stroke(PURPLE);
+    }
+    strokeWeight(4);
+    drawLayer(i);
+    pop();
+  }
+
+  function drawLayer(i) {
+    const d = i * 10;
+    // top triangle
+    push();
+    translate(0, -110);
+    triangle(-50 + d, 50 - d, 50 - d, 50 - d, 0, -80 + d);
+    pop();
+    // left triangle
+    push();
+    translate(-110, 0);
+    triangle(50 - d, 50 - d, -80 + d, 0, 50 - d, -50 + d);
+    pop();
+    // right triangle
+    push();
+    translate(110, 0);
+    triangle(-50 + d, 50 - d, 80 - d, 0, -50 + d, -50 + d);
+    pop();
+    // bottom triangle
+    push();
+    translate(0, 110);
+    triangle(-50 + d, -50 + d, 50 - d, -50 + d, 0, 250 - d);
+    pop();
+  }
 }
